0865989ab68cdbf5afb1ad638134e27ff2da6730
dude471sebas <dude471@hotmail.com>
2025-07-04 16:56:31 -0400

    Delete src directory

--- src/src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>EDM-TECH</h1>
-      <p>Precision EDM Solutions with Charmilles Robofil 240SL</p>
-    </main>
-  );
-}
