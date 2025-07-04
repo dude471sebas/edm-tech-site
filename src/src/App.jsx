b5c54fa7f17d9a8022abbc8476deb5fa34b94612
dude471sebas <dude471@hotmail.com>
2025-07-04 16:55:49 -0400

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>EDM-TECH</h1>
+      <p>Precision EDM Solutions with Charmilles Robofil 240SL</p>
+    </main>
+  );
+}
